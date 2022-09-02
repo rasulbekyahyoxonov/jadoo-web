3fe03f30859e30b958635d52b91766ad277dd6b4
rasulbekyahyoxonov <rasulbekyahyoxonov2@gmail.com>
2022-09-02 11:10:38 +0500

    tugadi!!!

--- js/javascrit.js
+++ js/javascrit.js
@@ -1,17 +1,17 @@
-// document.querySelector('.open-icon').addEventListener('click', () => {
-//     document.getElementById('title-ul').style.right = 0;
-//     document.getElementById('dark-div').style.visibility = 'visible';
-// })
-// document.querySelector('.close-icon').addEventListener('click', () => {
-//     document.getElementById('title-ul').style.right = '-370px';
-//     document.getElementById('dark-div').style.visibility = 'hidden';
+document.querySelector('.open-icon').addEventListener('click', () => {
+    document.getElementById('title-ul').style.right = 0;
+    document.getElementById('dark-div').style.visibility = 'visible';
+})
+document.querySelector('.close-icon').addEventListener('click', () => {
+    document.getElementById('title-ul').style.right = '-370px';
+    document.getElementById('dark-div').style.visibility = 'hidden';
 
-// })
-// document.querySelector('.dark-div').addEventListener('click', () => {
-//     document.getElementById('title-ul').style.right = '-370px';
-//     document.getElementById('dark-div').style.visibility = 'hidden';
+})
+document.querySelector('.dark-div').addEventListener('click', () => {
+    document.getElementById('title-ul').style.right = '-370px';
+    document.getElementById('dark-div').style.visibility = 'hidden';
 
-// })
+})
 
 ScrollReveal ({
   reset: false,
